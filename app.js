4446d93886998388a6e73dce63f3b9d1f2010725
Meraj Ali <96920684+MerajAli1@users.noreply.github.com>
2023-02-05 15:02:57 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,108 @@
+//--------------------------------- Chapter 14 to 17-----------------------------------------//
+// Qusetion no 1
+// var arr = []
+
+// Qusetion no 2
+// var arr = ["Meraj","Ali","Junaid","Wahaj","Kariz","Kamil"]
+
+
+// Qusetion no 3
+// var arr = [1,44,53,,56,78,56,45,3,34,75]
+
+
+// Qusetion no 4
+// var arr = [true, false]
+
+
+// Qusetion no 5
+// document.write("Qualification: </br>")
+// var arr = ["1)SSC</br>", "2)SSC</br>", "3)SSC</br>", "4)SSC</br>", "5)SSC</br>", "6)SSC</br>"]
+// var r=arr.join(" ")
+// document.write(r)
+
+// Qusetion no 6
+// var arr = ["Ali", "Meraj", "Daniyal"]
+// var arr2 = ["400", "299", "322"]
+// var total = arr2[0] / 500 * 100
+// var total2 = arr2[1] / 500 * 100
+// var total3 = arr2[2] / 500 * 100
+// document.write("Score of Ali is: " + arr2[0] + " Percentage is: " + total + "</br>")
+// document.write("Score of Meraj is: " + arr2[1] + " Percentage is: " + total2 + "</br>")
+// document.write("Score of Daniyal is: " + arr2[2] + " Percentage is: " + total3 + "</br>")
+
+
+// Question no 7
+// var arr = ["This", "is", "my", "cat"];
+// var r = arr.join(" ")
+// document.write(r)
+
+
+// Question no 7
+// var fruits = ["Strawbery", "Orange", "Apple", "Banana", "Watermelon"]
+// var r = fruits.sort()
+// document.write(r)
+
+
+// Question no 8
+// var compare = function(a,b){
+//     return a-b;
+// }
+// var fruits = [122, 324, 565, 563, 343, 453, 232]
+// var r = fruits.sort(compare)
+// document.write(r)
+
+
+
+
+//----------------------------------------- Chapter 18 to 20------------------------------//
+
+
+// Question no 1
+// for (i = 0; i < 11; i++) {
+// document.write("Hello world</br>")
+// }
+
+
+// Question no 2
+// for (i = 1; i < 11; i++) {
+// document.write(i,"</br>")
+// }
+
+
+// Question no 3
+// var table = prompt("Enter Table Number")
+// var range = prompt("Enter Range")
+// for (i = 1; i <=range ; i++) {
+//     document.write(table+"x"+i+"="+i*table+"</br>")
+// }
+
+
+// Question no 3
+// var arr = ["Nokia", "Samsung", "Q-Mobile", "Apple", "Sony"]
+// arr.push("Huwawe")
+// var r = arr.sort()
+// for (i = 0; i < arr.length; i++) {
+//     document.write(r[i], "</br>")
+// }
+
+
+// Question no 4
+// var fruits = ["Strawbery", "Orange", "Apple", "Banana", "Watermelon"]
+// var r = fruits.sort()
+// for (i = 0; i < fruits.length; i++) {
+//     document.write(r[i], "</br>")
+// }
+
+
+// Question no 5
+// var fruits = [rr]
+// var rr = prompt("Enter value")
+// for (i = 0; i < 10; i++) {
+//     document.write(fruits[i], "</br>")
+// }
+
+
+// Question no 6
+// for (i = 1; i <= 15; i++) {
+//     document.write(i,"</br>")
+// }
